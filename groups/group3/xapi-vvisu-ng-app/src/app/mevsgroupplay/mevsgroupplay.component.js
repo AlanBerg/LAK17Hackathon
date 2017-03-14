8200effa1104647666b5f6adc14cc0ad43689c0b
Mohamed Ez zaouia <ezzaouia.mohamed@gmail.com>
2017-03-14 20:02:48 +0100

    adding version 0 for the mevsgroupplay

--- groups/group3/xapi-vvisu-ng-app/src/app/mevsgroupplay/mevsgroupplay.component.js
+++ groups/group3/xapi-vvisu-ng-app/src/app/mevsgroupplay/mevsgroupplay.component.js
@@ -3,14 +3,16 @@ import template from './mevsgroupplay.component.html';
 import './mevsgroupplay.component.scss';
 
 import * as _ from 'lodash';
+import * as d3 from 'd3';
 
 class MeVsGroupController {
 
-    constructor($log, $state, $stateParams, $http) {
+    constructor($log, $state, $stateParams, $http, $element) {
         'ngInject'
         this.$log = $log.getInstance(MeVsGroupController.name);
         this.$http = $http;
         this.selectedLearnerId = '';
+        this.$element = this.$element;
     }
 
     log(...msg) {
@@ -30,7 +32,63 @@ class MeVsGroupController {
     }
 
     drawChart(selectedLearnerData, groupData) {
+        this.margin = { top: 10, left: 10, right: 10, bottom: 10 };
+        this.svgWidth = 700;
+        this.svgHeight = 700;
+        const RECTPAD = 5;
+        const RECTW = 20;
+        const GROUPMERECTPAD = 3;
+
+        // TODO to fix
+        let xScale = d3.scaleLinear().domain([0, this.svgWidth]).range([0, 100]);
+        let groupXScale = d3.scaleLinear().range([0 + this.svgWidth / 2 + GROUPMERECTPAD, this.svgWidth]).domain([0, 50]);
+        let meXScale = d3.scaleLinear().range([this.svgWidth / 2 - GROUPMERECTPAD, 0]).domain([0, 50]);
+        let colorScale = d3.scaleOrdinal(d3.schemeCategory20);
+
+
+        const data = [{ id: 1, d: 20 }, { id: 2, d: 30 }, { id: 3, d: 25 }];
+
+        this.svg = d3.select("#mevsgroupplay")
+            .attr('width', this.svgWidth)
+            .attr('height', this.svgHeight)
+            .append('g')
+            .attr('transform', `translate(${this.margin.left}, ${this.margin.top})`)
+
+        this.svg.selectAll('g')
+            .data(data)
+            .enter()
+            .append('g')
+            .attr('transform', (d, i) => {
+                return `translate(${0}, ${i * (RECTW + RECTPAD)})`
+            });
+
+        /** here goes the me bars charts */
+        this.svg.selectAll('g')
+            .append('line')
+            .attr('x1', meXScale(0))
+            .attr('x2', (d) => { return meXScale(d.d); })
+            .attr('y1', 0)
+            .attr('y2', 0)
+            .style('stroke', (d, i) => {
+                return colorScale(i);
+            })
+            .style('stroke-width', RECTW)
+
+        /** here goes the group bars charts */
+        this.svg.selectAll('g')
+            .append('line')
+            .attr('x1', groupXScale(0))
+            .attr('x2', (d) => { return groupXScale(d.d); })
+            .attr('y1', 0)
+            .attr('y2', 0)
+            .style('stroke', (d, i) => {
+                return colorScale(i);
+            })
+            .style('stroke-width', RECTW)
+
     }
+
+
 
     //
     applyFilter() {
@@ -68,7 +126,7 @@ class MeVsGroupController {
 
         this.log(learnerVideos, othersVideosGroupedByVideoId)
 
-
+        this.drawChart();
     }
 
 }
